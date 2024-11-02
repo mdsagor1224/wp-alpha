9d3a8dcc979700cfe6d4629136f94a2dd4455e59
mdsagor <mdsagorahmed1224@gmail.com>
2024-11-02 21:13:10 +0600

    alpha2

--- alpha/functions.php
+++ alpha/functions.php
@@ -1,5 +1,17 @@
 <?php 
 
-function change_case($text){
-    return strtoupper($text);
+function alpha_bootstraping(){
+    load_theme_textdomain("alpha");
+    add_theme_support("post-thumbnails");
+    add_theme_support("title-tag");
 }
+add_action("after_setup_theme","alpha_bootstraping");
+
+
+
+function alpha_assets(){
+    wp_enqueue_style("alpha", get_stylesheet_uri());
+    wp_enqueue_style("bootstrap","//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css");
+}
+
+add_action("wp_enqueue_scripts","alpha_assets");
